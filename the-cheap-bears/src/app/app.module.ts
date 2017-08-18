fcdd6bacaaa7d5f5c44a50fa78806d7103930a88
svrakata <svrakata@gmail.com>
2017-08-18 23:07:50 +0300

    firebase modules for Angular installed/apiKey set up

--- the-cheap-bears/src/app/app.module.ts
+++ the-cheap-bears/src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyB3qq6qjhHGl2Ha9vAccD329AEwLoY1KxM',
+    authDomain: 'thecheapbearsproject.firebaseapp.com',
+    databaseURL: 'https://thecheapbearsproject.firebaseio.com',
+    projectId: 'thecheapbearsproject',
+    storageBucket: '',
+    messagingSenderId: '864722532066'
+}
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +18,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
