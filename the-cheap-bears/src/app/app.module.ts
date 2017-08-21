938dbad46cebaf934bc3b7dcf1fbe433fcc799cc
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-08-21 19:10:53 +0300

    Firebase config data removed from main app module and firebaseConfig imported from environments.

--- the-cheap-bears/src/app/app.module.ts
+++ the-cheap-bears/src/app/app.module.ts
@@ -1,15 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
 
-export const firebaseConfig = {
-    apiKey: 'AIzaSyB3qq6qjhHGl2Ha9vAccD329AEwLoY1KxM',
-    authDomain: 'thecheapbearsproject.firebaseapp.com',
-    databaseURL: 'https://thecheapbearsproject.firebaseio.com',
-    projectId: 'thecheapbearsproject',
-    storageBucket: '',
-    messagingSenderId: '864722532066'
-}
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from './../environments/firebase.config'
 
 import { AppComponent } from './app.component';
 
