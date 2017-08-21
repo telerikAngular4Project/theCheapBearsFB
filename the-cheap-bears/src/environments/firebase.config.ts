2694c05461af57a7429564c3e9f45d6aa6976268
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-08-21 19:09:09 +0300

    Firebase config file added to environments.

---
+++ the-cheap-bears/src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB3qq6qjhHGl2Ha9vAccD329AEwLoY1KxM',
+  authDomain: 'thecheapbearsproject.firebaseapp.com',
+  databaseURL: 'https://thecheapbearsproject.firebaseio.com',
+  projectId: 'thecheapbearsproject',
+  storageBucket: '',
+  messagingSenderId: '864722532066'
+};
