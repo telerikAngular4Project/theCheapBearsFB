4cb86d021f0f9dd816d4efde1e4d72148a496966
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-09-08 01:46:55 +0300

    Filter-trips pipe added. Filter by cities, date and price.

---
+++ the-cheap-bears/src/app/trips/pipes/filter-trips.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterTripsPipe } from './filter-trips.pipe';
+
+describe('FilterTripsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterTripsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
