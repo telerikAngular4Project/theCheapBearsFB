bef81590c003249dd0fe73a1ccacd42e99c2e190
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-09-09 14:21:10 +0300

    trip-details component started.

--- the-cheap-bears/src/app/trips/trip-details/trip-details.component.ts
+++ the-cheap-bears/src/app/trips/trip-details/trip-details.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import 'rxjs/add/operator/mergeMap';
+
+import {DataService} from "../../shared/services/data.service";
+import {AuthService} from "../../shared/services/auth.service";
 
 @Component({
   selector: 'app-trip-details',
   templateUrl: './trip-details.component.html',
   styleUrls: ['./trip-details.component.css']
 })
+
 export class TripDetailsComponent implements OnInit {
+  tripId: string;
+  trip: any;
+  driver: any;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private activeRoute: ActivatedRoute,
+              private dataService: DataService,
+              private authService: AuthService) {
   }
 
+  ngOnInit() {
+    this.activeRoute
+      .queryParams.subscribe((params) => {
+      this.tripId = params['id'];
+    });
+
+    this.dataService.queryByKey('trips', this.tripId)
+      .flatMap(data => {
+        this.trip = data;
+        return this.dataService.queryByKey('users', this.trip.userId);
+      })
+      .subscribe((userFromDB) => {
+        this.driver = userFromDB;
+    });
+  };
+
+  reserveSeat() {
+
+
+    this.trip.freeSeats - 1;
+    this.trip.passengers.push()
+  }
 }
